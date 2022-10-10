7a12cb54fe8e4bc046e19995a8c041d408adc18d
Rorias <Larsmulderrr@gmail.com>
2022-10-10 08:43:27 +0200

    updated code conventions

--- JumperDoodle/Assets/Scripts/Mareyn Scripts/Command.cs
+++ JumperDoodle/Assets/Scripts/Mareyn Scripts/Command.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public abstract class Command
 {
-    public abstract void Execute(Animator anim);
+    public abstract void Execute(Animator _anim);
 }
 
 public class MoveLeft : Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator _anim)
     {
-        anim.SetTrigger("movingLeft");
+        _anim.SetTrigger("movingLeft");
     }
 }
 
 public class MoveRight : Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator _anim)
     {
-        anim.SetTrigger("movingRight");
+        _anim.SetTrigger("movingRight");
     }
 }
